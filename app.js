c224f10fe855169fca309e6db3880851bd31e69d
chaarlie37 <carlos37sm@gmail.com>
2020-08-06 14:50:35 +0200

    Section 5

--- app.js
+++ app.js
@@ -1,49 +1,32 @@
+// fSbEmxeJR9EyMr1H
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const Post = require('./models/post');
+const mongoose = require('mongoose');
+const postsRoutes = require('./routes/posts');
 
 const app = express();
+console.log('Server started.');
+
+mongoose.connect("mongodb+srv://charlie:fSbEmxeJR9EyMr1H@cluster0.mxpp6.mongodb.net/node-angular?retryWrites=true&w=majority")
+    .then(() => {
+        console.log('Connected to database!');
+    })
+    .catch(() =>{
+        console.error('Error!');
+    });
 
 app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
     next();
 });
 
 app.use(bodyParser.json());
 
-app.post('/api/posts', (req, res, next) =>{
-    const post = req.body;
-    console.log(post);
-    res.status(201).json({
-        message: 'Post added sucessfully.'
-    });
-});
+app.use('/api/posts/', postsRoutes);
 
-app.get('/api/posts', (req, res, next) => {
-    const posts = [
-        {
-            id: '1',
-            title: 'First server-side post',
-            content: 'dhgwiudhwihdudqwigqwi'
-        },{
-            id: '2',
-            title: '2 server-side post',
-            content: 'dhgwiudhwihdudqwigqwi'
-        },{
-            id: '3',
-            title: '3 server-side post',
-            content: 'dhgwiudhwihdudqwigqwi'
-        },{
-            id: '4',
-            title: '4 server-side post',
-            content: 'dhgwiudhwihdudqwigqwi'
-        }
-    ];
-    res.status(200).json({
-        message: 'Posts fetched succesfully!',
-        posts: posts
-    });
-});
 
 module.exports = app;
